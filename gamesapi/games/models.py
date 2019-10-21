ff8a08433062d6a0b1e1c33e2b4c013ded5cf7e4
Jonah Kuria <jonah.kuria@hotmail.com>
2019-10-21 09:13:49 +0200

    add game model

--- gamesapi/games/models.py
+++ gamesapi/games/models.py
@@ -8,3 +8,19 @@ class GameCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class Game(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, unique=True)
+    game_category = models.ForeignKey(
+        GameCategory,
+        related_name='games',
+        on_delete=models.CASCADE)
+    release_date = models.DateTimeField()
+    played = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
