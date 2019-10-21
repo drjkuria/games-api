3ff5b5da52bed80a7473682997a8986bb150e263
Jonah Kuria <jonah.kuria@hotmail.com>
2019-10-21 16:16:40 +0200

    add game list view

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+
+from games.models import Game
+from games.models import GameCategory
+
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-list'
+    filter_fields = (
+        'name',
+        'game_category',
+        'release_date',
+        'played',)
+    search_fields = (
+        '^name',)
+    ordering_fields = (
+        'name',
+        'release_date',)
+
+    def perform_create(self, serializer):
+        serializer.save()
