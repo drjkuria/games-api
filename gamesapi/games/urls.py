cf04248e7dd5cb4319da2b4b266b59fb8168b754
Jonah Kuria <jonah.kuria@hotmail.com>
2019-10-22 09:01:06 +0200

    add users urls

--- gamesapi/games/urls.py
+++ gamesapi/games/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
         name=views.GameCategoryList.name),
     path('game-categories/<int:pk>/', views.GameCategoryDetail.as_view(),
         name=views.GameCategoryDetail.name),
+    path('users/', views.UserList.as_view(),
+        name=views.UserList.name),
+    path('users/<int:pk>/', views.UserDetail.as_view(),
+        name=views.UserDetail.name),
     path('', views.ApiRoot.as_view(),
         name=views.ApiRoot.name),
 ]
