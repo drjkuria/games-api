24c7bb13e7c3bb45709074d611114df616f4c0c2
Jonah Kuria <jonah.kuria@hotmail.com>
2019-10-22 08:57:02 +0200

    add user list view

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from rest_framework import generics
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
+from rest_framework import permissions
 
 from games.models import Game
 from games.models import GameCategory
 from games.serializers import GameSerializer
 from games.serializers import GameCategorySerializer
+from games.serializers import UserSerializer
+from games.permissions import IsOwnerOrReadOnly
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    name = 'user-list'
 
 class GameList(generics.ListCreateAPIView):
     queryset = Game.objects.all()
