fd4abda94eb9772e16b85fd690c30c847fcf9223
Jonah Kuria <jonah.kuria@hotmail.com>
2019-10-23 07:14:45 +0200

    add player score filter

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -98,6 +98,32 @@ class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PlayerSerializer
     name = 'player-detail'
 
+class PlayerScoreFilter(FilterSet):
+    min_score = NumberFilter(
+        field_name='score', lookup_expr='gte')
+    max_score = NumberFilter(
+        field_name='score', lookup_expr='lte')
+    from_score_date = DateTimeFilter(
+        field_name='score_date', lookup_expr='gte')
+    to_score_date = DateTimeFilter(
+        field_name='score_date', lookup_expr='lte')
+    player_name = AllValuesFilter(
+        field_name='player__name')
+    game_name = AllValuesFilter(
+        field_name='game__name')
+
+    class Meta:
+        model = PlayerScore
+        fields = (
+            'score',
+            'from_score_date',
+            'to_score_date',
+            'min_score',
+            'max_score',
+            'player_name',
+            'game_name',
+            )
+
 class ApiRoot(generics.GenericAPIView):
     name = 'api-root'
     def get(self, request, *args, **kwargs):
